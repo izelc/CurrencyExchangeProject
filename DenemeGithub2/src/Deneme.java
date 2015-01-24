d4d6303355a399d1a0537951f9aab1608af0659d
izelc <izel.cavusoglu@hotmail.com>
2015-01-24 15:17:46 +0200

    Revert "Revert "Random sayi ""
    This reverts commit 1ac27704e6518da7567084541b2397c41f62fd2c.

--- DenemeGithub2/src/Deneme.java
+++ DenemeGithub2/src/Deneme.java
@@ -1,9 +1,17 @@
+import java.util.Random;
+
 
 public class Deneme {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("izel!!");
+		
+		
+	    Random rnd=new Random();
+	    
+	    int sayi = rnd.nextInt();
+	   System.out.println(sayi);
 	}
 
 }
